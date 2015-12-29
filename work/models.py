b98234f45a0e88a07b41d5500b681946ee23644a
Chris Heisel <chris@heisel.org>
2015-12-29 16:29:34 -0500

    Initial work models

--- work/models.py
+++ work/models.py
@@ -31,6 +31,47 @@ class WorkItem(models.Model):
         db_index=True,
         help_text="Reference to the item in your ticket tracker (JIRA Key, Story number, etc.)"
     )
+    issue_type = models.CharField(
+        max_length=255,
+        db_index=True,
+        help_text="Story, bug, epic, etc.",
+        null=False,
+        blank=False,
+    )
+    queued_at = models.DateTimeField(
+        null=False,
+        help_text="When did the team commit to doing the work item?"
+    )
+    started_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text="When did the team start working on the item?"
+    )
+    ended_at = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text="When did the team complete work on the item?"
+    )
+    cycle_time = models.DurationField(
+        null=True,
+        blank=True,
+        help_text="Time between ended_at and started_at"
+    )
+    lead_time = models.DurationField(
+        null=True,
+        blank=True,
+        help_text="Time between ended_at and queued_at"
+    )
+    effort_score = models.IntegerField(
+        null=True,
+        blank=True,
+        help_text="Story points, estimated hours, etc."
+    )
+    impact_score = models.IntegerField(
+        null=True,
+        blank=True,
+        help_text="Dollar value, bug impact, etc."
+    )
 
     class Meta:
         unique_together = ('key', 'dataset')
