19adda9f501b624ca7f09911a048d0d56ccc24bd
Chris Heisel <chris@heisel.org>
2016-02-14 10:45:50 -0500

    Revised model, added tests

--- work/models.py
+++ work/models.py
@@ -22,6 +22,7 @@ class WorkItem(models.Model):
     """
     Data about a story, defect, epic, etc.
     """
+    # Required fields
     dataset = models.ForeignKey(
         DataSet,
         on_delete=models.CASCADE
@@ -29,18 +30,21 @@ class WorkItem(models.Model):
     key = models.CharField(
         max_length=255,
         db_index=True,
-        help_text="Reference to the item in your ticket tracker (JIRA Key, Story number, etc.)"
+        help_text="Unique reference to the item in your ticket tracker (JIRA Key, Story number, etc.)"
     )
+    queued_at = models.DateTimeField(
+        null=False,
+        blank=False,
+        help_text="When did the team commit to doing the work item?"
+    )
+
+    # Optional fields
     issue_type = models.CharField(
         max_length=255,
         db_index=True,
         help_text="Story, bug, epic, etc.",
-        null=False,
-        blank=False,
-    )
-    queued_at = models.DateTimeField(
-        null=False,
-        help_text="When did the team commit to doing the work item?"
+        null=True,
+        blank=True,
     )
     started_at = models.DateTimeField(
         null=True,
@@ -51,16 +55,6 @@ class WorkItem(models.Model):
         null=True,
         blank=True,
         help_text="When did the team complete work on the item?"
-    )
-    cycle_time = models.DurationField(
-        null=True,
-        blank=True,
-        help_text="Time between ended_at and started_at"
-    )
-    lead_time = models.DurationField(
-        null=True,
-        blank=True,
-        help_text="Time between ended_at and queued_at"
     )
     effort_score = models.IntegerField(
         null=True,
@@ -73,8 +67,37 @@ class WorkItem(models.Model):
         help_text="Dollar value, bug impact, etc."
     )
 
+    # Computed fields
+    _cycle_time = models.DurationField(
+        null=True,
+        blank=True,
+        db_column="cycle_time",
+        help_text="Time between ended_at and started_at"
+    )
+    _lead_time = models.DurationField(
+        null=True,
+        blank=True,
+        db_column="lead_time",
+        help_text="Time between ended_at and queued_at"
+    )
+    _week_queued_at = models.DateField(
+        null=True,
+        blank=True,
+        help_text="The Sunday of the week the team committed to doing the work item?"
+    )
+    _week_started_at = models.DateField(
+        null=True,
+        blank=True,
+        help_text="The Sunday of the week the team start working on the item"
+    )
+    _week_ended_at = models.DateField(
+        null=True,
+        blank=True,
+        help_text="The Sunday of the week the team compelted working on the item"
+    )
+
     class Meta:
         unique_together = ('key', 'dataset')
 
     def __str__(self):
-        return u"{} / {}".format(self.key, self.dataset.name)
+        return u"{} / {}".format(self.dataset.name, self.key)
---
+++ work/tests/test_model_work.py
@@ -0,0 +1,31 @@
+import datetime
+import pytest
+
+pytestmark = pytest.mark.django_db
+
+@pytest.fixture()
+def make_one():
+    from work.models import WorkItem, DataSet
+    def _make_one(key, dataset="test_dataset", queued_at=None):
+        if queued_at is None:
+            queued_at = datetime.datetime.now()
+        d = DataSet(name=dataset, slug=dataset)
+        d.save()
+        w = WorkItem(
+            key=key,
+            dataset=d,
+            queued_at=queued_at
+        )
+        return w
+    return _make_one
+
+
+def test_setup(make_one):
+    w = make_one("TEST-1")
+    w.full_clean()
+
+
+def test_str(make_one):
+    w = make_one("TEST-1")
+    expected = "test_dataset / TEST-1"
+    assert expected == str(w)
